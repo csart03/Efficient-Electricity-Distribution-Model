202598c31707985ccd0b6c1550bca22045114829
Ar9020th <arthraj9@gmail.com>
2020-10-20 14:37:11 +0530

    Initial File

---
+++ graph.py
@@ -0,0 +1,9 @@
+class citytoken:
+	def __init__(self,x,y,label):
+		self.x=x
+		self.y=y
+		self.label=label
+
+citytokens=[]
+def constructgraph():
+	#this function will construct the graph and assign weights to 
